88b389ee5b4425189614db247ea18c6f50fdab04
pigrick <lai_yeerick@hotmail.com>
2015-02-23 20:48:46 -0600

    Java Fizz Buzz While Loop Program

---
+++ W1D1/FizzBuzzWhile.java
@@ -0,0 +1,19 @@
+public class FizzBuzzWhile{
+	public static void main(String[] args){
+		
+		int i = 1;
+		while(i != 101){
+			if(i % 3 == 0 && i % 5 == 0){
+				System.out.println(i + " FizzBuzz");
+			}else if(i % 3 == 0){
+				System.out.println(i + " Fizz");
+			}else if(i % 5 == 0){
+				System.out.println(i + " Buzz");
+			}else{
+				System.out.println(i);
+			}
+			i+=1;
+		}
+		
+	}
+}
